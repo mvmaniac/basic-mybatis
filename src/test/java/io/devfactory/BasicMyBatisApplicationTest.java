4efc762156c58ff9dff6d3aff9819cde302e21e3
yhs <mvmaniaz@gmail.com>
2024-10-30 23:36:27 +0900

    Update suppression warnings in test classes

--- src/test/java/io/devfactory/BasicMyBatisApplicationTest.java
+++ src/test/java/io/devfactory/BasicMyBatisApplicationTest.java
@@ -3,7 +3,7 @@ package io.devfactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SuppressWarnings("squid:S2699")
+@SuppressWarnings("squid:S1186")
 @SpringBootTest
 class BasicMyBatisApplicationTest {
 
@@ -12,4 +12,3 @@ class BasicMyBatisApplicationTest {
   }
 
 }
-
